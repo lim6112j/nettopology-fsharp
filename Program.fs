e003882c67b2c367845e495304b025e6bb69404b
lim6112j <lim6112j@gmail.com>
2023-11-28 00:58:15 +0900

    transform

--- Program.fs
+++ Program.fs
@@ -1,8 +1,11 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open GeoAPI.Geometries
+open GeoAPI.CoordinateSystems
 open NetTopologySuite.Geometries
 open NetTopologySuite.IO
 open NetTopologySuite.Operation.Distance
+open ProjNet.CoordinateSystems
+open ProjNet.CoordinateSystems.Transformations
 
 printfn "Hello from F#"
 
@@ -34,3 +37,35 @@ let distance2 =
 
 let distance3 =
     findClosestPoint ("LINESTRING (100 100, 300 300)", "POINT (140 280)")
+
+let projectGeom () =
+    let csFact = CoordinateSystemFactory()
+    let ctFact = CoordinateTransformationFactory()
+
+    let utm35ETRS =
+        csFact.CreateFromWkt(
+            "PROJCS[\"ETRS89 / ETRS-TM35\",GEOGCS[\"ETRS89\",DATUM[\"D_ETRS_1989\",SPHEROID[\"GRS_1980\",6378137,298.257222101]],PRIMEM[\"Greenwich\",0],UNIT[\"Degree\",0.017453292519943295]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",27],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0],UNIT[\"Meter\",1]]"
+        )
+
+    let utm33 = ProjectedCoordinateSystem.WGS84_UTM(33, true)
+    let wgs84 = GeographicCoordinateSystem.WGS84
+    let wgs84Utm33 = ProjectedCoordinateSystem.WGS84_UTM(33, true)
+    let trans = ctFact.CreateFromCoordinateSystems(utm35ETRS, wgs84)
+
+    let ilist (x: _ array) =
+        x :> System.Collections.Generic.IList<_>
+
+    let points =
+        ilist
+            [| [| 290586.087; 6714000.0 |]
+               [| 290586.392; 6713996.224 |]
+               [| 290590.133; 6713973.772 |]
+               [| 290594.111; 6713957.416 |]
+               [| 290596.615; 6713943.567 |]
+               [| 290596.701; 6713939.485 |] |]
+
+    let spoints = trans.MathTransform.Transform(290586.087, 6714000)
+    let tpoints = trans.MathTransform.TransformList(points)
+    tpoints
+
+printfn "%A" (projectGeom ())

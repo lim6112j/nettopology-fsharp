946fbcb821b3882c615a3ee408674d6fa375225d
ben lim <lim6112j@gmail.com>
2023-11-27 19:11:59 +0900

    initial commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open GeoAPI.Geometries
+open NetTopologySuite.Geometries
+open NetTopologySuite.IO
+open NetTopologySuite.Operation.Distance
+
+printfn "Hello from F#"
+
+let closestPoint () =
+    let g1 = WKTReader().Read("LINESTRING (0 0, 10 10, 20 20)")
+    let g2 = WKTReader().Read("LINESTRING (0 0, 10 10, 20 20)")
+    let g3 = g1.Intersection g2
+    g3.ToString()
+
+printfn "G1 intersection G2 : %s" (closestPoint ())
